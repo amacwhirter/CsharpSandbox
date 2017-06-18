6f61a57400a48ec94ec9b84980caf24959d88d37
Ashley MacWhirter <macwhirtera@gmail.com>
2017-06-17 23:57:42 -0400

    initial push

---
+++ Controllers/AppController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace App.Controllers
+{
+    public class AppController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.FirstName = "Ashley";
+            ViewBag.LastName = "MacWhirter";
+            return View();
+        }
+        public ActionResult About()
+		{
+            ViewBag.AppName = "First MVC .NET app... Woohoo!";
+			return View();
+		}
+        public ActionResult Contact()
+		{
+			return View();
+		}
+    }
+}
